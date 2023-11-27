e26ad968935ad87cb0696574f2ef32c668222470
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2023-11-27 14:09:42 -0700

    update

--- stats.py
+++ stats.py
@@ -14,4 +14,16 @@ from sympy import (
 
 from sympy.stats import P, E, variance, Die, Normal
 
+x = Symbol("x")
+y = Symbol("y")
+z = Symbol("z")
+t = Symbol("t")
+θ = Symbol("θ")
+φ = Symbol("φ")
+μ = Symbol("μ")
+u = Symbol("u")
+σ = Symbol("σ")
+v = Symbol("v")
+ρ = Symbol("ρ")
+r = Symbol("r")
 π = pi
