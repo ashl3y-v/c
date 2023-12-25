894b3a32bbaa7b671b0c8224856bb8e1c9df59d1
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2023-12-25 02:56:21 -0700

    update

--- polyzero/src/main.rs
+++ polyzero/src/main.rs
@@ -1,5 +1,3 @@
-#![feature(float_gamma)]
-
 use num_complex::Complex;
 use polynomial::Polynomial;
 use polyzero::roots;
@@ -9,37 +7,19 @@ use polyzero::roots;
 fn main() {
     let im = Complex::new;
 
-    im(0., 0.);
-
     let radii_coef = 2;
     let max_iter = 2usize.pow(12);
     let round_coef = 2_f64.powi(4);
     let eps = 2_f64.powi(2) * f64::EPSILON;
 
-    let n = 30;
-    let p = Polynomial::new(
-        (0..n)
-            .map(|n| match n % 2 {
-                0 => Complex::new(0., 0.),
-                _ => Complex::new(
-                    (-1f64).powi(n as i32 / 2) * (n as f64 + 1.).gamma().recip(),
-                    0.,
-                ),
-            })
-            .collect::<Vec<_>>(),
-    );
-
-    // let p = Polynomial::new(vec![
-    //     im(40320., 0.),
-    //     im(0., 109584.),
-    //     im(-118124., 0.),
-    //     im(0., -67284.),
-    //     im(22449., 0.),
-    //     im(0., 4536.),
-    //     im(-546., 0.),
-    //     im(0., -36.),
-    //     im(1., 0.),
-    // ]);
+    let p = Polynomial::new(vec![
+        im(-1., 0.),
+        im(-1., 0.),
+        im(0., 0.),
+        im(0., 0.),
+        im(0., 0.),
+        im(1., 0.),
+    ]);
 
     dbg!(&p);
 
