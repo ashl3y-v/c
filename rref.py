6d11e54aa46baceda754020b06d774c2e4ea39f3
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2024-02-29 22:37:30 -0700

    rref good

--- rref.py
+++ rref.py
@@ -5,19 +5,14 @@ def rref(a: np.ndarray) -> np.ndarray:
     rows = a.shape[-2]
 
     a = np.array(np.linalg.qr(a, mode="r"))
-    npad = np.array([(0, 0)] * a.ndim)
-    npad[-2] = (0, rows - a.shape[-2])
 
-    a = np.pad(a, pad_width=npad)
+    rank = np.count_nonzero(~np.isclose(np.diag(a), 0))
 
-    diag = np.diagonal(a, axis1=-2, axis2=-1).copy()
-    diag[np.isclose(diag, 0)] = 1
+    if rank == a.shape[-1]:
+        return np.eye(rows, a.shape[-1])
 
-    a[..., : diag.shape[-1], :] /= diag[..., :, np.newaxis]
+    a = np.linalg.inv(a[:rank, :rank]) @ a[:rank, :]
 
-    for i in range(1, min(a.shape[-2:])):
-        a[..., :i, i:] -= (
-            a[..., i, i:, np.newaxis] * a[..., np.newaxis, :i, i]
-        ).swapaxes(-1, -2)
+    a = np.vstack([a, np.zeros([rows - a.shape[-2], a.shape[-1]])])
 
     return a
