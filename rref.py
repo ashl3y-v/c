fc98a70b00f1dd83c521483cb176ff9c12ed701d
Ashley Vaughn <ashley.eva.vaughn@gmail.com>
2024-03-26 14:22:28 -0700

    update

--- rref.py
+++ rref.py
@@ -4,45 +4,36 @@ import numpy as np
 def rref(a: np.ndarray) -> np.ndarray:
     m, n = a.shape
 
-    r = np.linalg.matrix_rank(a)
-
-    if r == m:
-        return np.eye(m, n)
-
+    # QR decomposition
     a = np.linalg.qr(a, mode="r")
 
-    b = np.zeros_like(a)
-    b[0, 0] = 1
-
-    for i in range(n):
-        x = np.linalg.pinv(b[: i + 1, : i + 1]) @ b[: i + 1, i : i + 1]
-        print(x.shape)
-        x = np.pad(x, ((0, m - (i + 1)), (0, 0)))
-        print(b[:, : i + 1].T.shape, x.shape, b[: i + 1, i : i + 1].shape)
-        err = np.linalg.norm(b[:, : i + 1].T @ x - b[: i + 1, i : i + 1])
-        if err != 0:
-            x = np.zeros([m, 1])
-            x[i + 1] = 1
-            print(x)
-
-        b[:, i : i + 1] = x
-
-    # if r < m:
-    #     a = np.vstack([a, np.zeros([m - a.shape[-2], n])])
-
-    return b
-
-
-def rref_broken(a: np.ndarray) -> np.ndarray:
-    m, n = a.shape
-
-    a = np.linalg.qr(a, mode="r")
-
+    # calculate rank from number of zero diag elements
     r = np.count_nonzero(~np.isclose(np.diag(a), 0))
 
+    # return in special cases
     if r == n:
         return np.eye(m, n)
+    elif r == 0:
+        return np.zeros_like(a)
 
+    # normalize to get ones
+    first_in_rows = (a!=0).argmax(axis=-1)
+
+    scale = a[range(a.shape[-2]), first_in_rows]
+    scale[r:] = 1
+    a /= np.expand_dims(scale, axis=-1)
+
+    # calculate inverse of linearly independent subset
+    indep_subset = a[:, first_in_rows]
+    print(np.round(a, 3))
+    print(np.round(indep_subset, 3))
+
+    # apply inverse to dependent part
+
+    # replace independent part with identity
+    
+    # pad and return
+    
     a[:r, r:] = np.linalg.inv(a[:r, :r]) @ a[:r, r:]
     a[:r, :r] = np.eye(r)
 
@@ -54,10 +45,12 @@ def rref_broken(a: np.ndarray) -> np.ndarray:
     if r < m:
         a = np.vstack([a, np.zeros([m - a.shape[-2], n])])
 
+    a = np.triu(a)
+
     return a
 
 if __name__ == "__main__":
     a = np.random.random([5,5])
-    a[-1] = 0
-    b = rref(a)
-    print(b)
+    a[1] = a[0]
+
+    r = rref(a)
